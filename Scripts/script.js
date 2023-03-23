14c9bc804972f9063614151a4c61dc3840eb0ca6
roshan-renders <113288968+roshan-renders@users.noreply.github.com>
2023-03-23 12:37:31 +0530

    Add files via upload
    Organising  Files

---
+++ Scripts/script.js
@@ -0,0 +1,6 @@
+function w3_open() {
+    document.getElementById("mobile_nav").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mobile_nav").style.display = "none";
+  }
