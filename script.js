e083b098229e594418edad3b6dfc4dc168685ad5
roshan-renders <113288968+roshan-renders@users.noreply.github.com>
2023-03-23 12:36:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function w3_open() {
-    document.getElementById("mobile_nav").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("mobile_nav").style.display = "none";
-  }
